780971d50a221b9b9ed8423763b9bc940c09b41a
Maria Andrade <deise_andrade_paz@hotmail.com>
2024-12-08 22:01:10 -0300

    TrabalhandoComDataIO

--- src/Stream/TrabalhadoComDataIO.java
+++ src/Stream/TrabalhadoComDataIO.java
@@ -1,2 +1,65 @@
-package Stream;public class TrabalhadoComDataIO {
+package Stream;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TrabalhadoComDataIO {
+
+    public static void inculirProduto() throws IOException {
+
+        File arquivo = new File("C:\\Users\\Asus\\Documents\\peca-de-roupa.bin");
+
+        PrintStream ps = new PrintStream(System.out);
+        ps.flush();
+
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(arquivo));
+
+
+        Scanner sc = new Scanner(System.in);
+        ps.println("Nome da peça:");
+        String nomeDaPeca = sc.nextLine();
+        dos.writeUTF(nomeDaPeca);
+
+        ps.println("Tamanho da peça (P/M/G/U):");
+        char tamanhoDaPeca = sc.nextLine().charAt(0);
+        dos.writeChar(tamanhoDaPeca);
+
+        ps.println("Quantidade de peças:");
+        int quantidadeDePecas = sc.nextInt();
+        sc.nextLine(); // Limpa o buffer
+        dos.writeInt(quantidadeDePecas);
+
+        ps.println("Preço unitário:");
+        double precoUnitario = sc.nextDouble();
+        sc.nextLine(); // Limpa o buffer
+        dos.writeDouble(precoUnitario);
+
+        lerArquivo(arquivo.getPath());
+
+    }
+
+    public static void lerArquivo(String caminhoArquivo) throws IOException {
+
+        File arquivoParaLer = new File(caminhoArquivo);
+
+        DataInputStream dis = new DataInputStream(new FileInputStream(arquivoParaLer.getPath()));
+
+
+        String nome = dis.readUTF();
+        char tamanho = dis.readChar();
+        int quantidade = dis.readInt();
+        double preco = dis.readDouble();
+
+        System.out.printf("\nNome.................: %s\n", nome);
+        System.out.printf("\nTamaho.................: %s\n", tamanho);
+        System.out.printf("\nQuantidade.................: %s\n", quantidade);
+        System.out.printf("\nPreço.................: %s\n", preco);
+        System.out.printf("\nTotal do valor das peças.................: %s\n" + (quantidade * preco));
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        inculirProduto();
+    }
 }
--- src/Stream/TrabalhandoComBytes.java
+++ src/Stream/TrabalhandoComBytes.java
@@ -1,2 +1,19 @@
-package Stream;public class TrabalhandoComBytes {
+package Stream;
+
+import java.io.*;
+
+public class TrabalhandoComBytes {
+
+    public static void copiarArquivo() throws FileNotFoundException {
+
+        File file = new File("C:\\Users\\Asus\\Desktop\\Stream\\StreamJava\\recomendacoes.copy.txt");
+        String nomeArquivo = file.getName();
+        //Abre o arquivo
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file.getName()));
+
+        String nomeArquivoCopy = nomeArquivo.substring(0, nomeArquivo.indexOf(".")).concat("-cpy2.txt");
+        File fileCopy = new File(nomeArquivoCopy);
+
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(nomeArquivoCopy));
+    }
 }
